ee1343b5194e79976799954715295032cb01ddee
soumyadeepm04 <bmahapatra@c02xk3umjgh5.myfiosgateway.com>
2021-05-29 13:03:07 -0400

    added a new view

--- hello/views.py
+++ hello/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world") 
+
+def  soumyadeep(request)
+    return HttpResponse("Hello Soumyadeep")
