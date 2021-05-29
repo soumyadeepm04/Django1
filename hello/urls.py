1860f4ca601fcfb1c6ec0ec3439b7994ba60d86f
soumyadeepm04 <bmahapatra@c02xk3umjgh5.myfiosgateway.com>
2021-05-29 13:30:57 -0400

    correction to greet

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name = "index"),
     path("soumyadeep", views.soumyadeep, name = "soumyadeep"),
-    path("<str:name", views.greet, name = "greet")
+    path("<str:name">, views.greet, name = "greet")
 ]
